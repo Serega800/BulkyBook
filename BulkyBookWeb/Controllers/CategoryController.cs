b81faef35c3517a94d65be0b65cfc9ce07e8b728
Serega800 <svs3107@gmail.com>
2022-04-23 22:03:32 +0500

    added Create //POST IActionResult inside of CategoryController

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -16,11 +16,24 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
-        
+
         //GET
         public IActionResult Create()
-        {            
+        {
             return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
